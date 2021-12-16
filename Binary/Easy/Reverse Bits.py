338161ce8e9291835f1d5e1a9c0432cd4576d442
nmatthew45 <37429968+nmatthew45@users.noreply.github.com>
2021-12-16 01:14:09 -0800

    add dp problems

---
+++ Binary/Easy/Reverse Bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            bit = (n >> i) & 1
+            res = res | (bit << (31 - i))
+        return res
+
+# Time Complexity: O(1)
+# Space Complexity: O(1)
